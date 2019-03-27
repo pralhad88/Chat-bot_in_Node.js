f8730ba515ee80142505ffa9a10282f9ce173827
Pralhad Jadhav <41077041+pralhad88@users.noreply.github.com>
2019-03-27 09:24:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,39 +21,39 @@ var year = dateObj.getUTCFullYear();
 
 var todaysDate = day + "-" + monthName[month] + "-" + year;
 
-// if (todaysDate != data1[0]["Date"]){
-//     var stored_data = fs.readFileSync("daily_horoscope.json");
-//     var stored_data1 = JSON.parse(stored_data);
-//     stored_data1.push(data1);
-//     fs.writeFileSync("daily_horoscope.json", JSON.stringify(stored_data1, null, 2));
+if (todaysDate != data1[0]["Date"]){
+    var stored_data = fs.readFileSync("daily_horoscope.json");
+    var stored_data1 = JSON.parse(stored_data);
+    stored_data1.push(data1);
+    fs.writeFileSync("daily_horoscope.json", JSON.stringify(stored_data1, null, 2));
     
-//     i = 0;
-//     var data = [];
-//     while (true){
-//         request('https://www.ganeshaspeaks.com/horoscopes/daily-horoscope/' + signList[i] + '/', (err, resp, body) => {
-//             var dic = {};
-//             var $ = cheerio.load(body);
-//             var maindiv = $('p.margin-top-xs-0');
-//             var output = maindiv.text().trim();
-//             var date = $('div.daily-horoscope-content').find('p');
-//             var sign = $('a.breadcrumb').last().text().trim('\n');
-//             dic["sign"] = sign;
-//             dic["prediction"] = output;
-//             dic["Date"] = date.text();
-//             data.push(dic)
-//             fs.writeFileSync("horoscopes.json", JSON.stringify(data, null, 2));
-//         });
+    i = 0;
+    var data = [];
+    while (true){
+        request('https://www.ganeshaspeaks.com/horoscopes/daily-horoscope/' + signList[i] + '/', (err, resp, body) => {
+            var dic = {};
+            var $ = cheerio.load(body);
+            var maindiv = $('p.margin-top-xs-0');
+            var output = maindiv.text().trim();
+            var date = $('div.daily-horoscope-content').find('p');
+            var sign = $('a.breadcrumb').last().text().trim('\n');
+            dic["sign"] = sign;
+            dic["prediction"] = output;
+            dic["Date"] = date.text();
+            data.push(dic)
+            fs.writeFileSync("horoscopes.json", JSON.stringify(data, null, 2));
+        });
     
-//         if(request){
-//             i+=1
-//         }
+        if(request){
+            i+=1
+        }
     
-//         if(i == signList.length){
-//             break
-//         }
-//     }
+        if(i == signList.length){
+            break
+        }
+    }
 
-// }
+}
 
 
 app.get("/horoscope", (req,res) => {
